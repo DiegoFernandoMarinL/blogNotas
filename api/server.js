1a7dd123153cd9d8986d9d45c824c272b62e8111
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-10-25 08:53:53 -0500

    feat: :sparkles: Se crean nuevas api

--- api/server.js
+++ api/server.js
@@ -28,6 +28,43 @@ app.get('/notes', async (req, res) => {
     }
   });
 
+app.get('/notes/:id', async (req, res) => {
+  const id = req.params.id; 
+  try{
+      const db = await connectMongo();
+      const collection = db.collection('note');
+      const val = await collection.find({status: "activa"}).toArray();
+      console.log(val);
+      res.status(200).json(val);
+  }catch{
+      console.error('Error al obtener las notas', error);
+      res.status(500).send('Error en el servidor'); 
+  }
+});
+
+//Busca notas por título o contenido.
+app.get('/notes/search', async (req, res) => {
+  const text = req.body;
+  try {
+      const db = await connectMongo();
+      const collection = db.collection('note');
+      const val = await collection.findOne({
+        $or: [
+            { title: { $regex: text, $options: "i" } },
+            { content: { $regex: text, $options: "i" } }
+        ],
+        status: "activa"
+      },{projection: {title:1, content:1}});
+      if (val) {
+        res.status(200).json(val); 
+      } else {
+        res.status(204).send('Nota no encontrada'); 
+      }
+  } catch (error) {
+      console.error('Error al consultar la base de datos:', error);
+      res.status(500).json({ valido: false, mensaje: 'Error en el servidor' });
+  }
+});
 // Configurar el servidor HTTPS
 https.createServer(sslOptions, app).listen(3443, () => {
     console.log('Servidor HTTPS escuchando en el puerto 3443');
--- src/components/NotasUser.jsx
+++ src/components/NotasUser.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../styles/NotasUser.module.css'
 
 const NotasUser = ({ title }) => {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F7DC6F', '#8E44AD'];
+    const colors = ['#FD99FF', '#FF9E9E', '#91F48F', '#FFF599', '#9EFFFF', '#B69CFF'];
 
     function getRandomColorFromList() {
         const randomIndex = Math.floor(Math.random() * colors.length);
