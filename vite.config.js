d5241ac601aff915671fcefd25bb17ed65d8a690
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-10-29 07:26:31 -0500

    feat: :rocket: correcion despligue del frotend

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import fs from 'fs'
 export default defineConfig({
   plugins: [react()],
   base: 'https://diegofernandomarinl.github.io/blogNotas/',
-  server: {
+  /* server: {
     https: {
       key: fs.readFileSync('./server.key'),  // Ajusta la ruta a tu archivo .key
       cert: fs.readFileSync('./server.cert') // Ajusta la ruta a tu archivo .cert
     },
     host: 'localhost', // Opcional: Asegura que la configuración esté en el host correcto
     port: 3000, // Puerto para el servidor de desarrollo
-  }
+  } */
 })
